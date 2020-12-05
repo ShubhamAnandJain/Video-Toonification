fdc1708fedf1a3e5948f69eb9132a19c134db638
madhur76 <53146843+madhur76@users.noreply.github.com>
2020-12-05 15:48:18 +0530

    Updated with shot detection
    As Sent by Mithilesh with minor changes

--- code/myMainScript.m
+++ code/myMainScript.m
@@ -3,9 +3,9 @@ clear;
 %% Define all parameters for Toonification here
 %location of video file
 %location = 'xylophone.mp4';
-location = '../data/dubstepbird.mp4';
-save_inp_loc = '../data/in_bird1trial.mp4';
-save_loc = '../data/out_bird1trial.mp4';
+location = '../data/combined.mp4';
+save_inp_loc = '../data/in_combined.mp4';
+save_loc = '../data/out_comined.mp4';
 downsample_x = 4;
 downsample_y = 5;
 downsample_frame = 2;
@@ -41,9 +41,77 @@ disp("Video downsampled and ready for processing");
 disp(size(blankvid));
 toc;
 
-%% Does the actual Mean Shift segmentation
+%% Shot Detection
 tic;
 downsampled_vid = blankvid(:, :, :, 1:downsample_frame:end);
+boundaries = myShotDetection(downsampled_vid, 2.5);
+fprintf('Total shots = %i\n', (size(boundaries, 1)+1));
+%% Mean Shift Segmentation on multiple Videos
+start = 1;
+% add last frame as the end 
+boundaries = [boundaries, size(downsampled_vid, 4)];
+for segment = 1:size(boundaries, 1)
+    fprintf("On segment %i\n", segment);
+    cur_segment = downsampled_vid(:, :, :, start:boundaries(segment)-1);
+    mnshftvid = myMeanShiftSegmentation(cur_segment,spatial_sigma,intensity_sigma,time_sigma,num_iter,num_neighbor,lambda,windowsize,windowed,onlyy);
+    disp("Mean Shift segmentation done");
+    toc;
+    %% Canny Edge Addition
+    tic;
+    %mncannyvid = zeros(size(mnshftvid));
+    cannyvid=VideoWriter('../data/cannyvidtrial');
+    open(cannyvid);
+    for t =1:size(mnshftvid,4)
+        nxt_frame = mnshftvid(:,:,:,t)/ max(max(max(mnshftvid(:,:,:,t))));
+        for colors = 1:3
+            edge_mat = edge(nxt_frame(:,:,colors), 'canny', edge_threshold);
+            edge_mat = floor((256.0 / quantize) *edge_mat / max(max(max(edge_mat))))/256;%floor((256.0 / quantize) * 
+            imagesc(edge_mat);
+            nxt_frame(:,:,colors) = edge_lambda * nxt_frame(:,:,colors) + (1-edge_lambda) * edge_mat;
+        end
+        nxt_frame = nxt_frame/max(max(max(nxt_frame)));
+        writeVideo(cannyvid,nxt_frame)
+        %mncannyvid(:,:,:,t)=nxt_frame;
+    end
+    close(cannyvid);
+    disp("Video Canny has been saved");
+    toc;
+    %% final video chunk
+    final_vid(:, :, :, start:start+size(cur_segment, 4)-1) = mnshftvid;
+    %% Save Input Video for later comparisons
+    tic;
+    inputVid = VideoWriter(strcat(save_inp_loc, "_", num2str(segment)));
+    inputVid.FrameRate = vidObj.FrameRate;
+    open(inputVid);
+    for t = 1:size(blankvid, 4)
+        blankvid(:,:,:,t) = blankvid(:,:,:,t)/ max(max(max(blankvid(:,:,:,t))));
+        writeVideo(inputVid, blankvid(:, :, :, t));
+    end
+    close(inputVid);
+    disp("Video input has been saved");
+    toc;
+
+    %% Output Video is created
+    tic;
+    outputVid = VideoWriter(strcat(save_out_loc, "_", num2str(segment)));
+    % Due to downsampling, we have to modify framerate of output video
+    outputVid.FrameRate = vidObj.FrameRate * 1.0 / downsample_frame;
+    open(outputVid);
+    for t = 1:size(mnshftvid, 4)
+        %mnshftvid(:,:,:,t)= rescale(mnshftvid())
+        mnshftvid(:,:,:,t) = ((mnshftvid(:,:,:,t))/ max(max(max(mnshftvid(:,:,:,t)))));
+        writeVideo(outputVid, mnshftvid(:, :, :, t));
+    end
+    close(outputVid);
+    disp("Video output has been created");
+    % for next segment
+    start = boundaries(segment);
+end
+
+
+
+%% Does the actual Mean Shift segmentation
+tic;
 mnshftvid = myMeanShiftSegmentation(downsampled_vid,spatial_sigma,intensity_sigma,time_sigma,num_iter,num_neighbor,lambda,windowsize,windowed,onlyy);
 disp("Mean Shift segmentation done");
 toc;
